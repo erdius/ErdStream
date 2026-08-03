6c6ab000c98d42fc13c260a93c15206b49c9d5e2
David Erdman <david@Davids-Mac-mini.local>
2026-08-03 11:50:17 -0400

    Bump version to 0.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.erdman.erdstream"
         minSdk = 28
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.2.1"
+        versionCode = 13
+        versionName = "0.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
